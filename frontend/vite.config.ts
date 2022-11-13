6855dc2ed2f6766e72ae8932e204e3de704d2957
Philipp <philipp@f1ndus.de>
2022-11-13 04:27:38 +0100

    Web-Push test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [solidPlugin(), VitePWA({
-    injectRegister: 'auto'
-  })],
+  plugins: [solidPlugin()],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
